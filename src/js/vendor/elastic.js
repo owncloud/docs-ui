58b27953136fe365994f7a10a53e07f7c5efa01b
Thomas Boerger <thomas@webhippie.de>
2019-03-20 14:14:24 +0100

    Make the linter happy again

--- src/js/vendor/elastic.js
+++ src/js/vendor/elastic.js
@@ -1,71 +1,85 @@
-'use strict';
+'use strict'
 
-var $ = require('jquery');
-var Elasticsearch = require('elasticsearch-browser/elasticsearch');
-var Handlebars = require('handlebars');
+var $ = require('jquery')
+var Elasticsearch = require('elasticsearch-browser/elasticsearch')
+var Handlebars = require('handlebars')
 
-Handlebars.registerHelper('raw', function(options) {
-  return options.fn(this);
-});
+$('#search').on('keyup focus', function () {
+  var $search = $(this)
 
-$('#search').on('keyup focus', function() {
-  var $search = $(this);
+  var template = Handlebars.compile(
+    '{{#if error}}' +
+    '<p class="error">{{error}}</p>' +
+    '{{else}}' +
+    '<ul>' +
+    '{{#each matches}}' +
+    '<li><a href="{{url}}">' +
+    '<label>{{component}} :: {{version}}</label>' +
+    '<span class="score">Score {{score}}</span>' +
+    '<div class="head">{{head}}</div>' +
+    '<div class="body">{{body}}</div>' +
+    '</a></li>' +
+    '{{/each}}' +
+    '</ul>' +
+    '{{/if}}'
+  )
 
   if ($search.val().length >= 3) {
-    $('.search .animation').css('display', 'flex');
-    $('.search .results').html('');
-
-    var template = Handlebars.compile(
-      '{{#if error}}<p class="error">{{error}}</p>{{else}}<ul>{{#each matches}}<li><a href="{{url}}"><label>{{component}} :: {{version}}</label><span class="score">Score {{score}}</span><div class="head">{{head}}</div><div class="body">{{body}}</div></a></li>{{/each}}</ul>{{/if}}'
-    );
+    $('.search .animation').css('display', 'flex')
+    $('.search .results').html('')
 
     var client = new Elasticsearch.Client({
       host: $search.data('host'),
-      httpAuth: $search.data('auth')
-    });
+      httpAuth: $search.data('auth'),
+    })
 
     client.search({
-      q: $search.val()
+      q: $search.val(),
     }).then(function (body) {
       var context = {
         matches: [],
-        error: ''
-      };
+        error: '',
+      }
 
-      $.each(body.hits.hits, function(index, hit) {
+      $.each(body.hits.hits, function (index, hit) {
         context.matches.push({
           url: hit._source.url,
           component: hit._source.component,
-          version: hit._source.version == 'master' ? 'latest' : hit._source.version,
+          version: hit._source.version === 'master' ? 'latest' : hit._source.version,
           head: hit._source.title,
           body: hit._source.text.substr(0, 100) + ' ...',
-          score: Math.round(hit._score)
-        });
-      });
+          score: Math.round(hit._score),
+        })
+      })
 
-      if (context.matches.length == 0) {
+      if (context.matches.length === 0) {
         context.error = 'No matching result found'
       }
 
       $('.search .results')
         .html(template(context))
-        .show();
+        .show()
+
+      $('.search .animation').css('display', 'none')
     }, function (error) {
       if (window.console) {
-        console.trace(error.message);
+        console.trace(error.message)
       }
 
       var context = {
-        error: 'Failed to load results'
-      };
+        error: 'Failed to load results',
+      }
 
       $('.search .results')
         .html(template(context))
-        .show();
-    });
-  }
-});
+        .show()
 
-$('#search').on('blur', function() {
-  $('.search .results').hide('');
-});
+      $('.search .animation').css('display', 'none')
+    })
+  }
+})
+
+$('#search').on('blur', function () {
+  $('.search .results').hide('')
+  $('.search .animation').css('display', 'none')
+})
