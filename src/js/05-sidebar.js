3d5e4a6997c04fde19852d412d3eabfe12c50ba8
Lukáš Hirt <hirtlu@gmail.com>
2019-04-02 17:40:38 +0200

    Added trigger for the function when DOM content is loaded

--- src/js/05-sidebar.js
+++ src/js/05-sidebar.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var sidebar = document.querySelector('#sidebar');
-  var footer = document.querySelector('#footer');
-  var footerHeight;
-  var bodyHeight;
-  var windowSize;
-  var scrollPosition;
+var sidebar = document.querySelector('#sidebar');
+var footer = document.querySelector('#footer');
+var footerHeight;
+var bodyHeight;
+var windowSize;
+var scrollPosition;
 
-  window.addEventListener('scroll', function () {
-    scrollPosition = window.pageYOffset;
-    footerHeight = footer.clientHeight;
-    bodyHeight = document.body.offsetHeight;
-    windowSize = window.innerHeight;
-    distanceFromBottom = bodyHeight - (scrollPosition + windowSize);
-    if (distanceFromBottom <= footerHeight) {
-      sidebar.style.bottom = (footerHeight - distanceFromBottom) + "px";
-    } else {
-      sidebar.style.bottom = "0";
-    }
-  });
-})
+function checkSidebarPosition() {
+  scrollPosition = window.pageYOffset;
+  footerHeight = footer.clientHeight;
+  bodyHeight = document.body.offsetHeight;
+  windowSize = window.innerHeight;
+  distanceFromBottom = bodyHeight - (scrollPosition + windowSize);
+  if (distanceFromBottom <= footerHeight) {
+    sidebar.style.bottom = (footerHeight - distanceFromBottom) + "px";
+  } else {
+    sidebar.style.bottom = "0";
+  }
+}
+
+window.addEventListener('scroll', function() {
+  checkSidebarPosition();
+});
+document.addEventListener('DOMContentLoaded', function() {
+  checkSidebarPosition();
+});
