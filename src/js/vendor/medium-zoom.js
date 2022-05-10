bb89755fb2f56ccf97dfeeae1672f6201186f22f
mmattel <martin.mattel@diemattels.at>
2022-05-10 09:11:38 +0200

    Add package medium-zoom

---
+++ src/js/vendor/medium-zoom.js
@@ -0,0 +1,9 @@
+;(function () {
+  var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
+  // 'span.image img'
+  //    css selector that medium-zoom connects for standard images like jpg or png
+  // 'div.imageblock img'
+  //    css selector that medium-zoom connects for svg images
+  // with that, there is no need to define a role in the image for zooming - all images are zoomable !
+  Mzm('span.image img, div.imageblock img', { background: '#fff' })
+})()
