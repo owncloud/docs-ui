ecbbd20c37e7876c8fcb986a740e8b21c8b892e1
Lukas Hirt <info@hirt.cz>
2025-10-09 14:59:20 +0200

    chore: bump gulp-zip to v6.1.0

--- tasks/pack.js
+++ tasks/pack.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const gulp = require('gulp')
-const zip = require('gulp-zip')
+const zip = require('gulp-zip').default
 
 module.exports = (src, dest, bundleName) => new Promise((resolve, reject) => {
   gulp
     .src('**/*', { base: src, cwd: src })
     .pipe(zip(`${bundleName}-bundle.zip`))
     .pipe(gulp.dest(dest)).on('end', function() {
-      console.log('written')
+      console.log('Zip bundle created at', `${dest}/${bundleName}-bundle.zip`)
       resolve()
     })
     .on('error', function(err) {
